bc0bdc0cbcb78aee6c157e12d913ca00c1282dfc
kibetAmon <kibetngetich805@gmail.com>
2024-06-30 16:37:09 +0300

    Add skills section

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Backend = () => {
+  return (
+    <div className="skills_content">
+      <h3 className="skills_title">Backend developer</h3>
+      <div className="skills_box">
+        <div className="skills_group">
+          <div className="skills_data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills_name">Python</h3>
+              <span className="skills_level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills_data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills_name">Java</h3>
+              <span className="skills_level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills_data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills_name">Django</h3>
+              <span className="skills_level">Advanced</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="skills_group">
+          <div className="skills_data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills_name">C#</h3>
+              <span className="skills_level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills_data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills_name">C#</h3>
+              <span className="skills_level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills_data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills_name">Springboot</h3>
+              <span className="skills_level">Intermediate</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;
