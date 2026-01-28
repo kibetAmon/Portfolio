3881544ba6b7b0147e3c80776711f11aba309a3b
kibetAmon <kibetngetich805@gmail.com>
2026-01-28 10:29:56 +0300

    fix: mobile responsiveness fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 import React, { useState, useEffect } from "react";
 import "../styles/Header.css";
-import { CSSTransition } from "react-transition-group";
 import { Link } from 'react-scroll'
 
 
@@ -14,12 +13,13 @@ export default function Header() {
         
         const handleMediaQueryChange = (e) => {
             setIsSmallScreen(e.matches);
+            // Close menu when switching to desktop
+            if (!e.matches) {
+                setNavVisibility(false);
+            }
         };
         
-        // Modern way - use addEventListener
         mediaQuery.addEventListener('change', handleMediaQueryChange);
-        
-        // Set initial value
         handleMediaQueryChange(mediaQuery);
 
         return () => {
@@ -33,18 +33,13 @@ export default function Header() {
 
     return (           
         <header className="Header">
-            <CSSTransition
-                in={!isSmallScreen || isNavVisible}
-                timeout={350}
-                classNames="NavAnimation"
-                unmountOnExit
-            >
+            {(!isSmallScreen || isNavVisible) && (
                 <nav className="Nav">
                     <Link to="projects" spy={true} smooth={true} onClick={toggleNav} className="menu-link">Projects</Link>
                     <Link to="certifications" spy={true} smooth={true} onClick={toggleNav} className="menu-link">Certifications</Link>
                     <Link to="blog" spy={true} smooth={true} onClick={toggleNav} className="menu-link">Blogs</Link>
                 </nav>
-            </CSSTransition>
+            )}
             <button onClick={toggleNav} className="Burger">
                 <i className='fa fa-bars'></i>
             </button>
